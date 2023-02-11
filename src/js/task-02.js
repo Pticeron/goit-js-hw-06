f675ab8677a9d5a24eb09bbaf73142b7b45afe64
Inna Zorina <innazorina0101@gmail.com>
2023-02-11 19:04:49 +0200

    робить вправи

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector(`#ingredients`);
+console.log(ingredientsEl);
+
+const elements = ingredients.map(option => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.textContent = option;
+  ingredientEl.classList.add(`item`);
+  
+  return ingredientEl;
+});
+
+console.log(elements);
+
+ingredientsEl.append(...elements);
