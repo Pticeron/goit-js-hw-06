3524a6f00ae79a3d11a041a78a5befe7310ae1ac
Inna Zorina <innazorina0101@gmail.com>
2023-02-20 19:49:01 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -5,7 +5,8 @@ inputRef.addEventListener(`input`, onInputChange);
 
 function onInputChange(event) {
     // console.log(event.currentTarget.value);
-    outputRef.textContent = event.target.value;
+    // console.log(event.target.value);
+    outputRef.textContent = event.currentTarget.value;
     // inputRef != ` ` ? outputRef.textContent = event.currentTarget.value : `Anonymous`; 
     if(event.target.value === ``) {
         outputRef.textContent = 'Anonymous';
